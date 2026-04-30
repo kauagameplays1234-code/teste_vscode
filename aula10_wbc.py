c3a97cd27f78632826c3047353d0f35a3a966ef5
Kauã Borges <kauagameplays1234@gmail.com>
2026-04-29 21:38:33 -0300

    Add files via upload

---
+++ aula10_wbc.py
@@ -0,0 +1,115 @@
+# exemplo 01 break
+
+# contador = 1 
+# while contador <= 10:
+#     print(contador)
+    
+#     if contador == 5:
+#         break
+#     contador += 1
+
+#exemplo 02 break
+
+# senha = ""
+
+# while True:
+#     senha = input("digite a senha: ")
+
+#     if senha == "1234":
+#         print ("Acesso liberado")
+#         break
+    
+#     print("senha incorreta")
+
+#exemplo 03 continue
+
+# contador = 0
+
+# while contador < 10:
+#     contador += 1
+     
+#     if contador % 2 == 00:
+#         continue
+
+#     print(contador)
+
+#Atividade 01
+
+# contador = 1
+
+# while contador <= 10:
+#     print(contador)
+#     if contador == 6:
+#         break
+#     contador += 1
+
+#Atividade 02
+
+# contador = 1
+
+# while contador <= 10:
+#   print(contador)
+#   contador += 1
+#   if contador == 5:
+#      contador += 1
+#      continue
+    
+#Atividade 03
+
+# contador = 1
+
+# while contador <= 20:
+#     print(contador)
+#     if contador == 15:
+#         break
+#     if contador % 2 == 0:
+#         contador += 1
+#         continue
+#     print(contador)
+#     contador += 1
+    
+#Atividade 04
+
+# while True:
+#     produto = input("digite o nome do seu produto: ")
+    
+#     if produto == "fim":
+#         print("cadastro finalizado")
+#         break
+#     print("produto cadastrado: ",produto) 
+
+#Atividade 05
+
+# soma = 0 
+
+# while True:
+#     numero = int(input("digite um numero: "))
+#     soma += numero
+#     if soma == 20:
+#        break
+# print("total:", soma)
+
+
+#Atividade 06
+
+# soma = 0
+
+# while True:
+#     numero = int(input("digite um numero: "))
+#     soma += numero
+
+#     if soma >= 50:
+#         break
+# print("sua soma total foi: ", soma)
+
+#Atividade 07
+
+# senha = ""
+
+# while True:
+#     senha = input("digite a senha: ")
+
+#     if senha == "teste":
+#         print("acesso liberado")
+#         break
+#     print("senha incorreta")
